3f5954f0f345948b8140fcb75a6427b8327db9b6
Hubert Zub <hubert.zub@eyedea.io>
2019-09-05 12:10:02 +0200

    Updating relative paths in socket's package.json

--- scripts/compile-env.js
+++ scripts/compile-env.js
@@ -1,14 +1,39 @@
 const spawn = require('cross-spawn')
 const {resolve} = require('path')
-const {mkdirSync, copyFileSync} = require('fs')
+const {mkdirSync, readFileSync, writeFileSync} = require('fs')
 
 try {
   mkdirSync(resolve('.dist'))
 } catch (err) {}
 
 try {
-  copyFileSync(resolve('package.json'), resolve('.dist/package.json'))
-} catch (err) {}
+  const packageJsonFile = readFileSync(resolve('package.json'), {
+    encoding: 'utf-8'
+  })
+  const packageJson = JSON.parse(packageJsonFile)
+  const dependencyKeys = [
+    'dependencies',
+    'devDependencies',
+    'optionalDependencies'
+  ]
+  dependencyKeys.forEach(key => {
+    if (packageJson && typeof packageJson[key] === 'object') {
+      const dependencyObject = packageJson[key]
+      Object.keys(packageJson[key]).forEach(depName => {
+        if (dependencyObject[depName].indexOf('../') === 0) {
+          dependencyObject[depName] = `../${dependencyObject[depName]}`
+        }
+      })
+    }
+  })
+
+  writeFileSync(
+    resolve('.dist/package.json'),
+    JSON.stringify(packageJson, null, ' ')
+  )
+} catch (err) {
+  throw new Error('Malformed package.json file!')
+}
 
 const result = spawn.sync(
   'yarn',
