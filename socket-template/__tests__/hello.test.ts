a31827bf24d2b0efd9f9355289503e6dc7d90729
Kasper Mikiewicz <IderedPL@gmail.com>
2020-01-27 11:53:43 +0100

    chore: add better tests

--- socket-template/__tests__/hello.test.ts
+++ socket-template/__tests__/hello.test.ts
@@ -7,9 +7,17 @@ import {run} from '@eyedea/syncano-test'
 describe('resource/create', () => {
   it('should fail without firstname', async () => {
     const meta = {user: undefined}
-    const args = {id: 1}
-    const result = await run('profile', {args, meta})
+    const args = {}
+    const result = await run('hello', {args, meta})
 
     expect(result).toHaveProperty('code', 400)
+  })
+  it('should pass with firstname and lastname', async () => {
+    const meta = {user: undefined}
+    const args = {firstname: 'John', lastname: 'Doe'}
+    const result = await run('hello', {args, meta})
+
+    expect(result).toHaveProperty('data.message', 'Hello John Doe')
+    expect(result).toHaveProperty('code', 200)
   })
 })
